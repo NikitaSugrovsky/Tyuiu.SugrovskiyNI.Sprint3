cdcb987792e6739e970be82885904e6fcc3f08a3
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-10-18 20:11:43 +0500

    Task1

---
+++ Tyuiu.SugrovskiyNI.Sprint3.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SugrovskiyNI.Sprint3.Task1.V12.Lib;
+
+namespace Tyuiu.SugrovskiyNI.Sprint3.Task1.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 5;
+            int startValue = 1;
+            int stopValue = 10;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            double wait = 2.001;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
