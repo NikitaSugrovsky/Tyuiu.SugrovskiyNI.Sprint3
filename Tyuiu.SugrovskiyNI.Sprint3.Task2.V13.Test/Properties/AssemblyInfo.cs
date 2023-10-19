92076425b8200f07866e8bb0f3238c62d902df70
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-10-19 20:07:13 +0500

    Task2

---
+++ Tyuiu.SugrovskiyNI.Sprint3.Task2.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SugrovskiyNI.Sprint3.Task2.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SugrovskiyNI.Sprint3.Task2.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e66ab2af-2c99-4dc3-9450-7e0465a77e4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SugrovskiyNI.Sprint3.Task2.V13/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SugrovskiyNI.Sprint3.Task2.V13.Lib;
+
+namespace Tyuiu.SugrovskiyNI.Sprint3.Task2.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Сугровский Н. И. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла for                                                      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Сугровский Никита Игоревич | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Оператор цикла do-while                                                 *");
+            Console.WriteLine("* которая вычисляет произведение ряда по формуле                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 5;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("Переменная Х = " + value);
+            Console.WriteLine("Старт шага  = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Сумма рада = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
