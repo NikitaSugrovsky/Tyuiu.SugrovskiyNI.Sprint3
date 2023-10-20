a6f17714c71d1e68bd4aae5244ed657806d67d3f
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-10-20 22:47:43 +0500

    Task3

---
+++ Tyuiu.SugrovskiyNI.Sprint3.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SugrovskiyNI.Sprint3.Task3.V12.Lib;
+
+namespace Tyuiu.SugrovskiyNI.Sprint3.Task3.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMaxCharCount()
+        {
+            DataService ds = new DataService();
+
+            string value = "bkkrk ckkkcs ksr";
+            char item = 'k';
+
+            int res = ds.GetMaxCharCount(value, item);
+
+            int wait = 7;
+
+            Assert.AreEqual(wait, res);
+
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.SugrovskiyNI.Sprint3.Task3.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SugrovskiyNI.Sprint3.Task3.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SugrovskiyNI.Sprint3.Task3.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("36624163-c818-4dd1-b3ce-81fe51cff096")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
