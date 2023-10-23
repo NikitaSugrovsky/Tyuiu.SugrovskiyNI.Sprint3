7b36a15cba2ee3383adf8dd98ca0a2b8b223f23f
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-10-23 12:49:48 +0500

    Task4

---
+++ Tyuiu.SugrovskiyNI.Sprint3.Task4.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SugrovskiyNI.Sprint3.Task4.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SugrovskiyNI.Sprint3.Task4.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e316e69-a73f-4374-aa8b-8d976bba2513")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
