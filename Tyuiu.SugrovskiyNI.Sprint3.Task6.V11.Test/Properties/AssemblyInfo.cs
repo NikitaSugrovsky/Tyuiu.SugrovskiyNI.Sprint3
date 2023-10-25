5dfc82564a63a8cf8e80846f6eb033b8a8f7816e
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-10-25 16:20:48 +0500

    Task6

---
+++ Tyuiu.SugrovskiyNI.Sprint3.Task6.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SugrovskiyNI.Sprint3.Task6.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SugrovskiyNI.Sprint3.Task6.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77e0d339-b64e-4e27-a8a1-97a550f73194")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SugrovskiyNI.Sprint3.Task6.V11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SugrovskiyNI.Sprint3.Task6.V11.Lib;
+
+namespace Tyuiu.SugrovskiyNI.Sprint3.Task6.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                DataService ds = new DataService();
+
+                Console.Title = "Спринт #3 | Выполнил: Сугровский Н. И. | ИИПб-23-1";
+                Console.WriteLine("******************************************************************************");
+                Console.WriteLine("* Спринт #3                                                                  *");
+                Console.WriteLine("* Обработка целочисленной информации                                         *");
+                Console.WriteLine("* Задание #6                                                                 *");
+                Console.WriteLine("* Вариант #11                                                                *");
+                Console.WriteLine("* Выполнил: Сугровский Никита Игоревич | ИИПб-23-1                           *");
+                Console.WriteLine("******************************************************************************");
+                Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+                Console.WriteLine("* Программа, которая ищет среди целых чисел, принадлежащих числовому отрезку *");
+                Console.WriteLine("* [10, 19] количество всех делителей больше 10                              * ");
+                Console.WriteLine("*                                                                            *");
+                Console.WriteLine("******************************************************************************");
+                Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+                Console.WriteLine("******************************************************************************");
+
+                int startValue = 10;
+                int stoptValue = 19;
+
+
+
+                Console.WriteLine("Начало отрезка = " + startValue);
+                Console.WriteLine("Конец отрезка = " + stoptValue);
+
+
+                Console.WriteLine("******************************************************************************");
+                Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+                Console.WriteLine("******************************************************************************");
+
+                Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stoptValue));
+
+                Console.ReadKey();
+            }
+    }
+}
